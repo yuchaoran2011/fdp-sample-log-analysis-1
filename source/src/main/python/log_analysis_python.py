49ebbcd27601a4cac6fb152519503b2f16836b94
Chaoran Yu <yuchaoran2011@gmail.com>
2017-02-13 16:32:08 -0500

    Added comments

--- source/src/main/python/log_analysis_python.py
+++ source/src/main/python/log_analysis_python.py
@@ -1,5 +1,13 @@
 %spark.pyspark
 
+"""
+Log analysis application that processes data in Apache server log format.
+Example datasets can be found in:
+http://ita.ee.lbl.gov/html/contrib/ClarkNet-HTTP.html
+This code is intended to be executed interactively in a Zeppelin session.
+Please specify input file (i.e. log_file variable) before running.
+"""
+
 import datetime
 import os
 import re
@@ -11,7 +19,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-log_file = '/Users/stephen/Desktop/misc/clarknet_access_log_Sep4'
+
+# Specify input file here
+log_file = 'path_to_input_file'
 
 APACHE_LOG_REGEX = '^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S*)" (\d{3}) (\S+)'
 months = {'Jan': 1, 'Feb': 2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8,  'Sep': 9, 'Oct':10, 'Nov': 11, 'Dec': 12}
@@ -59,10 +69,13 @@ def parse_apache_logs():
 
 parsed_logs, access_logs, failed_logs = parse_apache_logs()
 
+# Output statistics on request payload sizes
 payload_sizes = access_logs.map(lambda log: log.payload_size).cache()
 payload_size_avg = payload_sizes.reduce(lambda a, b : a + b) / payload_sizes.count()
 print 'Payload Size Avg: %i, Min: %i, Max: %s' % (payload_size_avg, payload_sizes.min(), payload_sizes.max())
 
+
+# Generate variables for use in plotting
 status_counts = access_logs.map(lambda log: (log.status, 1)).reduceByKey(lambda a, b: a + b).cache()
 labels = status_counts.map(lambda (x, y): x).collect()
 count = access_logs.count()
@@ -84,6 +97,7 @@ for text, autotext in zip(texts, autotexts):
         text.set_text('')
 plt.legend(labels, loc=(0.80, -0.1), shadow=True)
 
+# Output any 10 hosts that appeared more than 20 times
 ip_counts = access_logs.map(lambda log: (log.ip, 1))
 host_more_than_20 = ip_counts.reduceByKey(lambda a, b : a + b).filter(lambda s: s[1] > 20)
 hosts_pick_10 = host_more_than_20.map(lambda s: s[0]).take(10)
